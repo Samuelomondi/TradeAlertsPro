21a9bc3a5775105b35bfa18dd787a84cd76eaf55
Samuel Omondi <samuelomondi3@gmail.com>
2025-09-20 21:01:49 +0000

    menu button onclick should be purple and also the on hover

--- src/components/ui/menubar.tsx
+++ src/components/ui/menubar.tsx
@@ -58,7 +58,7 @@ const MenubarTrigger = React.forwardRef<
   <MenubarPrimitive.Trigger
     ref={ref}
     className={cn(
-      "flex cursor-default select-none items-center rounded-sm px-3 py-1.5 text-sm font-medium outline-none focus:bg-accent focus:text-accent-foreground data-[state=open]:bg-accent data-[state=open]:text-accent-foreground",
+      "flex cursor-default select-none items-center rounded-sm px-3 py-1.5 text-sm font-medium outline-none hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground data-[state=open]:bg-primary data-[state=open]:text-primary-foreground",
       className
     )}
     {...props}
